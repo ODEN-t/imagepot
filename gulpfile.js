c9e725341bb8cf30a7489f64ba11200d8d63483e
takuma <mrbig8192@gmail.com>
2021-01-10 00:39:32 +0900

    gulpfile.js 改良

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    minifyCss = require('gulp-minify-css'),
-    plumber = require('gulp-plumber');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss')
 
-gulp.task('sass', function () {
+
+gulp.task('sass', (done) => {
     gulp.src('assets/stylesheets/*.scss')
         .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(minifyCss())
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            extname: '.min.css'
+        }))
         .pipe(gulp.dest('src/main/resources/static/css'))
+    done();
+});
+
+gulp.task('default', () => {
+    gulp.watch('assets/stylesheets/*.scss', gulp.task('sass'));
 });
