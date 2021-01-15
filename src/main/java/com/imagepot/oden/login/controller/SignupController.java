41aa54a827da0a341da33e1e17f00de7f3b62f4d
takuma <mrbig8192@gmail.com>
2021-01-15 23:01:51 +0900

    signup.html

---
+++ src/main/java/com/imagepot/oden/login/controller/SignupController.java
@@ -0,0 +1,13 @@
+package com.imagepot.oden.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignupController {
+    @GetMapping("/signup")
+    public String getSignup(Model model) {
+        return "signup";
+    }
+}
