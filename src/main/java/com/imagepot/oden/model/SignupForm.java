edaf97821a8dea0310f2f910112bded8b37a6971
takuma <mrbig8192@gmail.com>
2021-03-18 15:58:06 +0900

    signupForm colum変更、エラーメッセージ

--- src/main/java/com/imagepot/oden/model/SignupForm.java
+++ src/main/java/com/imagepot/oden/model/SignupForm.java
@@ -1,12 +1,25 @@
 package com.imagepot.oden.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
 public class SignupForm {
+    @NotBlank
+    @Length(min = 6, max = 10)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String loginId;
+
+    @NotBlank
+    @Length(min = 8, max = 10)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
-    private String firstName;
-    private String lastName;
-    private String nickName;
+
+    @NotBlank
+    @Length(min = 3, max = 10)
+    private String userName;
 }
