8fbb0fc1285ff48c301a3773955fe2dae7974e09
takuma <mrbig8192@gmail.com>
2021-03-22 07:35:43 +0900

    トップページ modalFooter追加

--- src/main/resources/static/js/top/index.js
+++ src/main/resources/static/js/top/index.js
@@ -2,26 +2,12 @@ import jBox from 'jbox';
 import 'jbox/dist/jBox.all.css';
 import $ from 'jquery/dist/jquery.min';
 
-
-
-
-// $('#signup').on('click', function () {
-//     console.log('click!f!');
-// })
-
-
-
-// let b = document.getElementsByClassName('buttonCTA');
-// console.log(b);
-
 new jBox('Modal', {
-    width: 470,
-    height: 500,
+    width: 405,
+    height: 573,
     attach: '#signup',
     content: $('#modalContent'),
     overlayClass: 'add-jboxOverRay',
     closeOnClick: false,
     closeButton: 'box'
 });
-
-// console.log("log");
