cac75e71d24184ff9bc5cedf1a9620d2aa3df4f6
takuma <mrbig8192@gmail.com>
2021-04-07 11:09:39 +0900

    signout 導線設定

--- src/main/resources/static/js/common/index.js
+++ src/main/resources/static/js/common/index.js
@@ -2,11 +2,6 @@ import jBox from 'jbox';
 import 'jbox/dist/jBox.all.css';
 import $ from 'jquery/dist/jquery.min';
 
-const ass = document.getElementById('signout');
-ass.addEventListener('click', () => {
-    console.log("click");
-})
-
 // Signup Modal の設定値
 const signoutPopup = new jBox('Confirm', {
     title: 'Sign Out',
