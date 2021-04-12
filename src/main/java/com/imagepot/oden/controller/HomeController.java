2f07e3fc982dee43ea48c264d39ead62bc289a29
takuma <mrbig8192@gmail.com>
2021-04-12 14:30:24 +0900

    adminフロント変更, adminデータバインド

--- src/main/java/com/imagepot/oden/controller/HomeController.java
+++ src/main/java/com/imagepot/oden/controller/HomeController.java
@@ -1,13 +1,34 @@
 package com.imagepot.oden.controller;
 
+import java.util.List;
+
+import com.imagepot.oden.model.User;
+import com.imagepot.oden.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService userService;
+
     @GetMapping("/home")
     public String getHome(Model model) {
         return "home";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(Model model) {
+
+        List<User> userList = userService.selectAllUser();
+        model.addAttribute("userList", userList);
+
+        int count = userList.size();
+        model.addAttribute("count", count);
+        return "admin";
     }
 }
