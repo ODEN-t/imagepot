e7a14b82454e8ad76c844c682e6dae1225a1d63e
takuma <mrbig8192@gmail.com>
2021-05-17 19:46:34 +0900

    home aws s3 -> 画像表示実装

--- src/main/resources/static/js/home/index.js
+++ src/main/resources/static/js/home/index.js
@@ -1,157 +1,97 @@
 import $ from 'jquery/dist/jquery.min';
 
-const generateImageTemplateList = () => {
-    let templateList = [];
+/***
+ * 引数に渡したテンプレートを引数numberの数だけ配列に詰め込み返す
+ * @param {String} template 要素を表現したテンプレートリテラル
+ * @param {Number} number 量産したいtemplateの数
+ * @return []
+ */
+const generateImageTemplateList = (template, number) => {
+    const templateList = [];
     const domParser = new DOMParser();
-
-    for (const img of imageList) {
-        let template =
-            `<figure>
-                <div class="p-home__postedList__imgWrap">
-                </div>
-            </figure>`;
+    let i = 0;
+    while(i < number) {
         templateList.push(domParser.parseFromString(template, "text/html").body.firstChild);
+        i++;
     }
     return templateList;
 }
 
 /**
- *　引数に渡した配列を指定した数毎にHTMLElementでラップしHTML要素として追加する
+ *　引数に渡した配列を指定した数毎にラップしHTML要素として追加する
  *
- * @param listOfElement HTMLElementを詰めた配列
- * @param unit 何個ずつwrapしたいかを指定
- * @return map
+ * @param {Array} listOfElement HTMLElementを詰めた配列
+ * @param {Number} unit 何個ずつwrapしたいかを指定
+ * @param {String} wrapperTemplate wrapする親要素を表現したテンプレートリテラル
+ * @return void
  */
-const WrapNodesInUnit = (listOfElement, unit, imageList) => {
+const WrapNodesInEachUnit = (listOfElement, unit, wrapperTemplate) => {
     const contents = document.getElementById('js-postedList');
-    const NUMBER_OF_ELEMENTS = listOfElement.length;
-    const NUMBER_OF_ELEMENTS_IN_SECTION = unit;
-    const NUMBER_OF_SECTIONS = Math.ceil(NUMBER_OF_ELEMENTS / NUMBER_OF_ELEMENTS_IN_SECTION);
+    const NUMBER_OF_ELEMENTS = listOfElement.length; // 要素の数
+    const NUMBER_OF_WRAPPERS = Math.ceil(NUMBER_OF_ELEMENTS / unit); // 要素をラップするラッパーの合計数
     const domParser = new DOMParser();
-    let section = 0;
-    let eachNum = unit;
+    let wrapNumber = 0;
+    let eachNum = unit;　// 各ラッパー内に追加する要素数
     let elemNum = 0;
-    const map = new Map;
-    let tmp = [];
 
-    console.log("NUMBER_OF_SECTIONS -> " + NUMBER_OF_SECTIONS);
-    console.log("NUMBER_OF_ELEMENTS -> " + NUMBER_OF_ELEMENTS)
+    while (wrapNumber <= NUMBER_OF_WRAPPERS) {
 
-    while (section <= NUMBER_OF_SECTIONS) {
-        let template =
-            `<div class="p-home__postedList__section" data-interSection="${section}"></div>`;
-        let wrapper = domParser.parseFromString(template, "text/html").body.firstChild;
+        let wrapper = domParser.parseFromString(wrapperTemplate, "text/html").body.firstChild;
 
         while (elemNum < eachNum) {
             if (NUMBER_OF_ELEMENTS <= elemNum) break;
             wrapper.appendChild(listOfElement[elemNum]);
-            tmp.push(imageList[elemNum]);
             elemNum++;
         }
-        map.set(`section${section}`, tmp);
-        tmp = [];
+
         contents.appendChild(wrapper);
 
-        // if(section === 1) break;
-
         if (NUMBER_OF_ELEMENTS <= elemNum) break;
-        eachNum += NUMBER_OF_ELEMENTS_IN_SECTION;
-        section++;
+        eachNum += unit;
+        wrapNumber++;
     }
-
-    return map;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const UNIT = 15;
-    const map = WrapNodesInUnit(generateImageTemplateList(), UNIT, imageList);
-    console.log(map);
-
-
-    let parent = document.querySelector('[data-intersection="0"]');
-    let element = document.querySelectorAll('[data-intersection="0"] .p-home__postedList__imgWrap');
-    const NUMBER_OF_IMAGES = element.length;
-    let counter = 0;
-
-    console.log(parent.querySelectorAll('.p-home__postedList__imgWrap'));
+    const wrapperTemplate = `<div class="p-home__postedItem js-intersect"></div>`;
+    const childTemplate = `<figure><div class="p-home__postedImg"></div></figure>`;
+    const UNIT = 15; // 一度にrenderする画像の数
+    WrapNodesInEachUnit(generateImageTemplateList(childTemplate, imageList.length), UNIT, wrapperTemplate);
 
     const options = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.15
-    }
+        threshold: 0.25
+    };
 
     const intersectionObserver = new IntersectionObserver(renderImage, options);
-    function renderImage(changes) {
-        changes.forEach(change => {
-            if(change.isIntersecting)
-                console.log("intersect!");
-        })
-    }
+    document.querySelectorAll('.js-intersect').forEach((e) => {
+        intersectionObserver.observe(e);
+    });
 
-    for (let i = 0; i < NUMBER_OF_IMAGES; i++) {
-        const img = document.createElement('img');
-        img.src = imageList[i];
-        element[i].appendChild(img);
-        img.addEventListener('load', function () {
-            counter++;
-            if(counter === NUMBER_OF_IMAGES) {
-                parent.classList.add('is-loaded');
-                console.log("load" + counter);
-                let elem = document.querySelectorAll('.p-home__postedList__section');
-                elem.forEach((e) => {
-                    intersectionObserver.observe(e);
-                })
+    // IntersectionObserverのメインロジック
+    // スクロール毎にimgタグを生成し画像を表示
+    function renderImage(entries) {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                let parent = entry.target;
+                let children = parent.querySelectorAll('.p-home__postedImg');
+                let count = 0;
+                for(let i = 0; i < children.length; i++) {
+                    const img = document.createElement('img');
+                    img.src = imageList[i];
+                    children[i].appendChild(img);
+
+                    img.addEventListener('load', () => {
+                        imageList.shift();
+                        count++;
+                        if(count === children.length)
+                            parent.classList.add('is-loaded');
+                    });
+                }
+
+                intersectionObserver.unobserve(entry.target);
             }
         })
     }
-
-
-})
-
-// const func = () => {
-//     const target = document.getElementById('js-masonry');
-//     const domList = generateTemplateList();
-//     // console.log(domList);
-//
-//     for(const img of domList) {
-//         target.appendChild();
-//     }
-//
-//
-//     const options = {
-//         root: null,
-//         rootMargin: 0,
-//         threshold: [0.2, 1.0]
-//     }
-//
-//     const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             console.log(entry);
-//             console.log(entry.isIntersecting);
-//             console.log(entry.intersectionRatio);
-//         }, options);
-//     });
-//
-//     observer.observe(target);
-//
-//
-//     // let i = 0;
-//     // const max = 30;
-//     //
-//     // const loadContent = async () => {
-//     //
-//     //     contents.appendChild(domParser.parseFromString(domList[i], "text/html").body.firstChild);
-//     //
-//     //     i++;
-//     //
-//     //     if ( i < max )
-//     //         infiniteScrollObserver.observe(contents.lastElementChild);
-//     //
-//     // };
-//
-//     //
-//     // loadContent();
-// }
-//
-// func();
+});
