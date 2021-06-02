88e466f7bc855e6d792bbbf6be80077f74a1ce17
takuma <mrbig8192@gmail.com>
2021-06-03 00:44:14 +0900

    masonry導入

--- src/main/resources/static/js/home/index.js
+++ src/main/resources/static/js/home/index.js
@@ -1,45 +1,30 @@
-// import * as module from '../module/index';
-//
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//     const wrapperTemplate = `<div class="p-home__postedItem js-intersect"></div>`;
-//     const childTemplate = `<figure><div class="p-home__postedImg"></div></figure>`;
-//     const UNIT = 15; // 一度にrenderする画像の数
-//     module.WrapNodesInEachUnit(module.generateImageTemplateList(childTemplate, urlList.length), UNIT, wrapperTemplate);
-//
-//     const options = {
-//         root: null,
-//         rootMargin: '0px',
-//         threshold: 0.25
-//     };
-//
-//     const intersectionObserver = new IntersectionObserver(renderImage, options);
-//     document.querySelectorAll('.js-intersect').forEach((e) => {
-//         intersectionObserver.observe(e);
-//     });
-//
-//     // IntersectionObserverのメインロジック
-//     // スクロール毎にimgタグを生成し画像を表示
-//     function renderImage(entries) {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 let parent = entry.target;
-//                 let children = parent.querySelectorAll('.p-home__postedImg');
-//                 let count = 0;
-//                 for (let i = 0; i < children.length; i++) {
-//                     const img = document.createElement('img');
-//                     img.src = urlList[i];
-//                     children[i].appendChild(img);
-//                     img.addEventListener('load', () => {
-//                         urlList.shift();
-//                         count++;
-//                         if (count === children.length)
-//                             parent.classList.add('is-loaded');
-//                     });
-//                 }
-//                 intersectionObserver.unobserve(entry.target);
-//             }
-//         })
-//     }
-// });
+import Masonry from 'masonry-layout/masonry';
+import imagesLoaded from 'imagesloaded/imagesloaded';
+
+console.log('from home');
+
+const loadingMask = document.querySelector('.loadingMask');
+const container = document.querySelector('.p-home__postedItem');
+const imgLoaded = imagesLoaded(container);
+
+imgLoaded.on('always', () => {
+    console.log('loaded always');
+})
+
+imgLoaded.on('done', () => {
+    console.log('done');
+    let msnry = new Masonry( '.p-home__postedItem', {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: '.gutter-sizer',
+        horizontalOrder: true,
+        percentPosition: true,
+    });
+
+    msnry.on('layoutComplete', function () {
+        loadingMask.classList.add('is-loaded');
+    });
+
+    msnry.layout();
+})
 
